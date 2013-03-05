84fcb5d79ef6f74a00e3ce1ee8c04a09cccffc07
Dewey Sasser <dewey@sasser.com>
2013-03-05 09:48:11 -0500

    Adding my maps on using freemind life dashboard/todo and project managmeent.

---
+++ doc/Freemind for project management.mm
@@ -0,0 +1,17 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#ff0000" CREATED="1361970515904" ID="ID_509149016" LINK="My%20Life.mm" MODIFIED="1361970645515" TEXT="Freemind for project management">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1361970522783" ID="ID_221993862" MODIFIED="1361970523748" POSITION="right" TEXT="Current">
+<node CREATED="1361970553015" ID="ID_113385318" MODIFIED="1361970556383" TEXT="This is the &quot;current sprint&quot;"/>
+</node>
+<node CREATED="1361970521046" ID="ID_187090446" MODIFIED="1361970522611" POSITION="right" TEXT="Backlog">
+<node CREATED="1361970544440" ID="ID_461700249" MODIFIED="1361970547618" TEXT="Items on the backlog list"/>
+<node CREATED="1361970547794" ID="ID_1617279931" MODIFIED="1361970549746" TEXT="Go Here"/>
+<node CREATED="1361970563876" ID="ID_595710480" MODIFIED="1361970574537" TEXT="Quesiton:  How do I do sizing estimates?"/>
+<node CREATED="1361970585277" ID="ID_1601795468" MODIFIED="1361970593085" TEXT="Question:  How to automatically track/graph/etc"/>
+</node>
+<node CREATED="1361970524577" ID="ID_981025444" MODIFIED="1361970525372" POSITION="left" TEXT="Notes"/>
+<node CREATED="1361970525568" ID="ID_1814772240" MODIFIED="1361970526909" POSITION="left" TEXT="Resources"/>
+</node>
+</map>
